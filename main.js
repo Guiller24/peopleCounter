2b0aa753a849539a1d58f120a9920d8099d2ae46
Guiller24 <sumgueg@gmail.com>
2022-12-03 10:10:29 +0800

    Rewrote the whole code

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-reactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"))
+let count = 0
+let countdis = document.getElementById("count")
+let lbl = document.getElementById("saved-count")
+let addButton = document.getElementById("increment-btn")
+let saveButton = document.getElementById("save-btn")
+
+addButton.addEventListener("click", () =>{
+    count++
+    countdis.innerText = count
+})
+
+saveButton.addEventListener("click", () =>{
+    lbl.innerText += " " + count + " - "
+    count = 0
+    countdis.innerText = count
+})
+
+
+
+
+
 
